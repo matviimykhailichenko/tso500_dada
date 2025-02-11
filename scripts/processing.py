6eca10e9fcd85aa599edd445564900507d2dd267
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-02-11 10:00:11 +0100

    Processing crontab

---
+++ scripts/processing.py
@@ -0,0 +1,109 @@
+from pathlib import Path
+import yaml
+import subprocess
+
+
+
+def is_server_available(server_idle_tag: Path,
+                        server_busy_tag: Path) -> bool:
+    try:
+        if Path(server_busy_tag).exists() and not Path(server_idle_tag).exists():
+            return True
+        return False
+    except Exception as e:
+        # TODO discord bot?
+        raise RuntimeError(f"Failed to check server status: {e}")
+
+
+def process_run(run_type: str,
+                base_dir: Path,
+                onco_dir: Path,
+                cbmed_dir: Path,
+                server_idle_tag: Path,
+                server_busy_tag: Path,
+                pending_tag: str):
+    try:
+        if run_type == 'oncoservice':
+            pending_tag_path = onco_dir / pending_tag
+            run_files_dir_path = Path(pending_tag_path).read_text()
+            results_dir_path = onco_dir/ Path(run_files_dir_path).name
+
+        elif run_type == 'cbmed':
+            pending_tag_path = cbmed_dir / pending_tag
+            run_files_dir_path = Path(pending_tag_path).read_text()
+            results_dir_path = cbmed_dir / Path(run_files_dir_path).name
+
+        else:
+            raise RuntimeError(f"Unrecognised run type") # TODO log
+
+        server_idle_tag.unlink()
+        snakefile_path = str(base_dir / 'snakefile')
+        config_file_path = str(base_dir / 'config.yaml')
+        # TODO not sure if we need this if this is going to be reported to the bot
+        # analysing_tag = Path(run_dir) / analysing_tag
+        # analysing_tag.touch()
+        server_busy_tag.touch()
+        # TODO discord bot
+
+        try:
+            subprocess.run([ # TODO check if it runs on full cores
+                "conda", "run", "-n", "tso_500_dragen_pipeline",
+                "snakemake", "-s", snakefile_path,
+                "--configfile", config_file_path,
+                "--config", f"run_files_dir_path={run_files_dir_path}", f'results_dir_path={results_dir_path}', f'run_type={run_type}'
+            ])
+        except Exception as e:
+            raise RuntimeError(f"Error processing run {run_files_dir_path}: {e}")
+
+        print(f'Processing run {run_files_dir_path}')
+        server_busy_tag.unlink()
+        server_idle_tag.touch()
+        pending_tag_path.unlink()
+    except Exception as e:
+        raise RuntimeError(f"Error processing run {run_files_dir_path}: {e}")
+
+
+def check_pending_runs(pending_onco_tag: Path,
+                    pending_cbmed_tag: Path):
+    if pending_onco_tag.exists():
+        return 'oncoservice'
+    elif pending_cbmed_tag.exists():
+        return 'cbmed'
+    else:
+        # TODO change to logging
+        print('No Oncoservice or CBmed runs are detected, quitting...')
+
+
+
+def main():
+    # Definitions
+    with open('../config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+        base_dir = config['base_dir']
+        onco_dir = Path(base_dir) / config['oncoservice_dir']
+        cbmed_dir = Path(base_dir) / config['cbmed_dir']
+        pending_tag = config['pending_run_tag']
+        pending_onco_tag = Path(onco_dir / config['pending_run_tag'])
+        pending_cbmed_tag = Path(cbmed_dir / config['pending_run_tag'])
+        server_availability_dir = Path(base_dir) / config['server_availability_dir']
+        server_idle_tag = server_availability_dir / config['server_idle_tag']
+        server_busy_tag = server_availability_dir / config['server_busy_tag']
+
+    if is_server_available(server_idle_tag=server_idle_tag,
+                           server_busy_tag=server_busy_tag):
+        return
+
+    run_type = check_pending_runs(pending_onco_tag=pending_onco_tag,
+                               pending_cbmed_tag=pending_cbmed_tag)
+
+    # TODO check an assumption that there would not be 2 runs of one type
+    process_run(run_type=run_type,
+                base_dir=base_dir,
+                onco_dir=onco_dir,
+                cbmed_dir=cbmed_dir,
+                server_idle_tag=server_idle_tag,
+                server_busy_tag=server_busy_tag,
+                pending_tag=pending_tag)
+
+if __name__ == "__main__":
+    main()
