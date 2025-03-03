d657992a70ba41bcef4ff7c299559783f057e392
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-03-03 11:19:03 +0100

    Sandboxing error handling

--- sandbox/process_run/test.py
+++ sandbox/process_run/test.py
@@ -1,13 +1,13 @@
-import subprocess
+from shutil import which as sh_which
 
-process_call = ['process.sh']
-try:
-    subprocess.run(process_call).check_returncode()
-except subprocess.CalledProcessError as e:
-    message = f"Process failed with a return code: {e.returncode}."
-    raise RuntimeError(message)
-except FileNotFoundError:
-    message = f"Process failed. Script not found"
-    raise RuntimeError(message)
+rsync_path = sh_which('rsync')
+if rsync_path is None:
+    raise FileNotFoundError
+print('Rsync was foynd')
 
-print('Ze process has run succesfulliah')
+rsync_path = sh_which('rsynk')
+
+if rsync_path is None:
+    raise FileNotFoundError
+
+print("Rsunk wasn't found")
