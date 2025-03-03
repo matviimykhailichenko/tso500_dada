6ee0372d9fd10c0829301b5c1c5a504f4c4c6d9c
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-03-03 11:11:19 +0100

    Sandboxing error handling

--- sandbox/process_run/test.py
+++ sandbox/process_run/test.py
@@ -1,10 +1,13 @@
 import subprocess
 
-process_call = ['snakemake', '-j1', '-s', '/mnt/Novaseq/TSO_pipeline/02_Development/testing/process_run/test']
+process_call = ['process.sh']
 try:
     subprocess.run(process_call).check_returncode()
 except subprocess.CalledProcessError as e:
     message = f"Process failed with a return code: {e.returncode}."
+    raise RuntimeError(message)
+except FileNotFoundError:
+    message = f"Process failed. Script not found"
     raise RuntimeError(message)
 
 print('Ze process has run succesfulliah')
