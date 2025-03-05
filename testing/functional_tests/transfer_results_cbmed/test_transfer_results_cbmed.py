580a71c30d51f583ac2913a8de878e40af80fe16
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-03-05 10:00:01 +0100

    Functionally testing transferring results to CBmed dirs

--- testing/functional_tests/transfer_results_cbmed/test_transfer_results_cbmed.py
+++ testing/functional_tests/transfer_results_cbmed/test_transfer_results_cbmed.py
@@ -1,3 +1,9 @@
 from scripts.helpers import transfer_results_cbmed
+from scripts.logging_ops import setup_logger
 
-transfer_results_cbmed()
+logger_runtime = setup_logger('transfer_results_cbmed',
+                              '/mnt/Novaseq/TSO_pipeline/02_Development/testing/functional_tests/transfer_results_cbmed/last_execution.log')
+transfer_results_cbmed(flowcell='test_run_files_dir',
+                       rsync_path_str='/usr/bin/rsync',
+                       run_name='test_results_dir',
+                       logger=logger_runtime)
