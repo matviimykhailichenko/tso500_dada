b5f3ffd607c51c067a9e25d575759aa7dfeda93f
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-03-05 10:19:48 +0100

    Functionally testing transferring results to CBmed dirs: sandboxig creatig dirs

--- testing/functional_tests/transfer_results_cbmed/test_transfer_results_cbmed.py
+++ testing/functional_tests/transfer_results_cbmed/test_transfer_results_cbmed.py
@@ -3,8 +3,11 @@ from scripts.logging_ops import setup_logger
 
 logger_runtime = setup_logger('transfer_results_cbmed',
                               '/mnt/Novaseq/TSO_pipeline/02_Development/testing/functional_tests/transfer_results_cbmed/last_execution.log')
-transfer_results_cbmed(flowcell='test_run_files_dir',
+try:
+    transfer_results_cbmed(flowcell='test_run_files_dir',
                        rsync_path_str='/usr/bin/rsync',
                        run_name='test_results_dir',
                        logger=logger_runtime,
                        testing=True)
+except Exception as e:
+    print(e)
