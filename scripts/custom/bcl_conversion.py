0e8801afd650203d2dfea2ccefc9e5bf77d5b3ac
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-03-17 10:39:34 +0100

    [Fix] using TSO500 bash script to demux

--- scripts/custom/bcl_conversion.py
+++ scripts/custom/bcl_conversion.py
@@ -1,9 +1,8 @@
-from subprocess import run as subp_run, SubprocessError, CalledProcessError
-from tabnanny import check
+from subprocess import run as subp_run, CalledProcessError
 
-dragen_call = ("dragen --bcl-conversion-only true "
-               "--bcl-input-directory /mnt/Novaseq/07_Oncoservice/Runs/250314_TSO500_Onco/250314_A01664_0475_AHVF33DSXC "
-               "--output-directory /mnt/Novaseq/07_Oncoservice/Runs/250314_TSO500_Onco/250314_A01664_0475_AHVF33DSXC/FastqGeneration")
+dragen_call = ("DRAGEN_TSO500.sh --resourcesFolder /staging/illumina/DRAGEN_TSO500/resources --hashtableFolder /staging/illumina/DRAGEN_TSO500/ref_hashtable --runFolder /mnt/Novaseq/07_Oncoservice/Runs/250314_TSO500_Onco/250314_A01664_0475_AHVF33DSXC"
+               " --analysisFolder /mnt/Novaseq/07_Oncoservice/Runs/250314_TSO500_Onco/FastqGeneration"
+               " --demultiplexOnly --sampleSheet /mnt/Novaseq/07_Oncoservice/Runs/250314_TSO500_Onco/250314_A01664_0475_AHVF33DSXC/SampleSheet.csv")
 
 try:
     subp_run(dragen_call, check=True)
