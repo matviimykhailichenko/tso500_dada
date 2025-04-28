0e9d6b82690ebf17dd0c4c4a0185d8431d8285b9
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-04-28 10:20:23 +0200

    -

--- testing/functional_tests/processing/test.py
+++ testing/functional_tests/processing/test.py
@@ -3,12 +3,18 @@ from pathlib import Path
 from scripts.helpers import get_server_ip
 from shutil import copy as sh_copy
 from subprocess import run as subp_run
+import yaml
+
 
 @pytest.fixture()
 def setup_environment():
+    with open('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+        pipeline_dir: Path = Path(config['pipeline_dir'])
+
     server_ip = get_server_ip()
-    queue_file = Path('/mnt/Novaseq/TSO_pipeline').parent.parent / f'{server_ip}_QUEUE.txt'
-    pending_file = Path('/mnt/Novaseq/TSO_pipeline').parent.parent / f'{server_ip}_PEDNING.txt'
+    queue_file = pipeline_dir.parent.parent / f'{server_ip}_QUEUE.txt'
+    pending_file = pipeline_dir.parent.parent / f'{server_ip}_PEDNING.txt'
 
     sh_copy('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/testing/functional_tests/processing/PENDING.txt',pending_file)
 
