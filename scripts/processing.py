df13d8ada658bdb6ad2c01a703aeefbd657568b6
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-04-29 09:49:45 +0200

    Staging stuff

--- scripts/processing.py
+++ scripts/processing.py
@@ -124,7 +124,7 @@ def main():
 
                 queue_no_processing.to_csv(queue_file, sep='\t', index=False)
 
-                    pending_file.write_text('')
+                pending_file.write_text('')
 
                 pass
 
