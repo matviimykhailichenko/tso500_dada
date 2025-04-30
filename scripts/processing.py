c1e41f73c73d25f1871098f2d067f54b7d6b46e6
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-04-30 11:16:24 +0200

    Transferring_stuff

--- scripts/processing.py
+++ scripts/processing.py
@@ -93,7 +93,7 @@ def main():
 
     queue = get_queue(pending_file=pending_file, queue_file=queue_file)
 
-    if queue in [None, '']:
+    if queue is None or queue.empty:
         return
 
     path, input_type, _, tag, flowcell = queue.iloc[0]
