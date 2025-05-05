4b948ead1c96c7be75b993b587cbbf4e593646d4
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-05-05 14:18:44 +0200

    Testing onco runs

--- testing/integration_tests/mock/test_oncoservice_runs.py
+++ testing/integration_tests/mock/test_oncoservice_runs.py
@@ -37,5 +37,5 @@ def setup_environment():
 
 def test_processing(setup_environment):
     processing_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py'
-
-    subp_run(processing_call,check=True,shell=True)
+    for i in range(2):
+        subp_run(processing_call,check=True,shell=True)
