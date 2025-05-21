b17e15d389a09ecad338dd85ba070cc383bef8e8
Matvii Mykhailichenko <103883982+matviimykhailichenko@users.noreply.github.com>
2025-05-21 14:58:56 +0200

    Mock testing processing CBM runs

--- testing/integration_tests/mock/test_CBmed_runs.py
+++ testing/integration_tests/mock/test_CBmed_runs.py
@@ -26,16 +26,16 @@ def setup_environment():
         queue_file.touch()
 
     if not pending_file.exists():
-        sh_copy(str(test_pending_file),str(pending_file))
+        sh_copy(str(test_pending_file), str(pending_file))
 
     if not test_cbmed_run_seq_dir_1.exists():
-        sh_copytree(str(test_cbmed_run_1),str(test_cbmed_run_seq_dir_1))
+        sh_copytree(str(test_cbmed_run_1), str(test_cbmed_run_seq_dir_1))
 
     if not test_cbmed_run_seq_dir_2.exists():
         sh_copytree(str(test_cbmed_run_2), str(test_cbmed_run_seq_dir_2))
 
 
-def test_processing(setup_environment):
-    processing_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py'
-    for i in range(2):
-        subp_run(processing_call,check=True,shell=True)
+# def test_processing(setup_environment):
+#     processing_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py'
+#     for i in range(2):
+#         subp_run(processing_call,check=True,shell=True)
