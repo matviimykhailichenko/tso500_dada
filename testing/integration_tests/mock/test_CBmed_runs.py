12079c70917a3c641d607c04ad1aaf94a109a810
Matvii Mykhailichenko <103883982+matviimykhailichenko@users.noreply.github.com>
2025-05-21 15:00:10 +0200

    Mock testing processing CBM runs

--- testing/integration_tests/mock/test_CBmed_runs.py
+++ testing/integration_tests/mock/test_CBmed_runs.py
@@ -35,7 +35,9 @@ def setup_environment():
         sh_copytree(str(test_cbmed_run_2), str(test_cbmed_run_seq_dir_2))
 
 
-# def test_processing(setup_environment):
-#     processing_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py'
-#     for i in range(2):
-#         subp_run(processing_call,check=True,shell=True)
+def test_processing(setup_environment):
+    processing_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py'
+    # for i in range(2):
+    #     subp_run(processing_call,check=True,shell=True)
+
+    print('fuck')
