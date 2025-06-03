9c79b62d3ae467561605ae61f837c9745bb3bfd3
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-06-03 09:56:39 +0200

    Mock testing scheduler.py

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -46,9 +46,9 @@ def main():
         exit(0)
 
     if input_type == 'run':
-        append_pending_run(input_dir=input_path, testing=testing)
+        append_pending_run(paths=paths, input_dir=input_path, testing=testing)
     elif input_type == 'sample':
-        append_pending_samples(input_dir=input_path, sample_ids=sample_ids, testing=testing)
+        append_pending_samples(paths=paths, input_dir=input_path, sample_ids=sample_ids, testing=testing)
     else:
         RuntimeError(f'Unrecognised input type: {input_type}')
 
