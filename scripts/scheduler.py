8ac4105494c67577c60e0f872da66b92b65a9fa2
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-06-03 09:36:16 +0200

    Mock testing scheduler.py

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -34,8 +34,8 @@ def main():
     input_type = None
     sample_ids = None
     for dir in seq_dirs:
+        notify_bot(str(dir))
         if sx182_mountpoint in str(dir):
-            notify_bot(str(dir))
             input_type = 'run'
             input_path = scan_dir_nsq6000(seq_dir=dir)
 
