c724b3c1764b74692823713d90e51b485cd5fd5a
Matvii Mykhailichenko <103883982+matviimykhailichenko@users.noreply.github.com>
2025-06-04 16:24:45 +0200

    Sandboxing comparing checksums

--- sandbox/compare_checksums/bar.py
+++ sandbox/compare_checksums/bar.py
@@ -1,9 +1,9 @@
 import subprocess
 
 
-file1_checksum = '/Users/matviimykhailichenko/Library/CloudStorage/OneDrive-UniwersytetWroclawski/Carrier/Projects/Bioinformatics/tso500_dragen_pipeline/sandbox/compare_checksums/dir/1.sha256'
-file2_checksum = '/Users/matviimykhailichenko/Library/CloudStorage/OneDrive-UniwersytetWroclawski/Carrier/Projects/Bioinformatics/tso500_dragen_pipeline/sandbox/compare_checksums/dir/2.sha256'
+file1_checksum = '/Users/matviimykhailichenko/Documents/tso500_dragen_pipeline/sandbox/compare_checksums/dir/1.sha256'
+file2_checksum = '/Users/matviimykhailichenko/Documents/tso500_dragen_pipeline/sandbox/compare_checksums/dir/2.sha256'
 
 compare_checksum_call = f'sha256sum {file1_checksum} {file2_checksum}'
 
-subprocess.run(compare_checksum_call, check=True)
+subprocess.run(compare_checksum_call, shell=True, check=True)
