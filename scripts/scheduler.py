4d1c892a5e5ee0687f596525940fbb407b3b41b4
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-06-06 12:24:21 +0200

    Stupid error

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -26,7 +26,7 @@ def main():
     sample_ids = None
     for dir in seq_dirs:
         if (str(paths['sx182_mountpoint']) in str(dir)) or (str(paths['patho_seq_dir']) in str(dir)):
-            notify_bot(dir)
+            notify_bot(str(dir))
             input_type = 'run'
             input_path = scan_dir_nsq6000(seq_dir=dir)
 
