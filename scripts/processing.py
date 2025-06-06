66e49ef3933430aa207ddc645f50ddb3f1fdd30c
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-06-06 13:57:00 +0200

    Stupid error

--- scripts/processing.py
+++ scripts/processing.py
@@ -36,7 +36,6 @@ def main():
     if not queue_file.exists() or queue_file.stat().st_size < 38:
         queue_blank = Path('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/testing/functional_tests/scheduler/PENDING_blank.txt')
         sh_copy(queue_blank,queue_file)
-        return
 
     queue = get_queue(pending_file=pending_file, queue_file=queue_file)
 
