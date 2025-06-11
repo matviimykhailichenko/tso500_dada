2d04634f5877647942c0993d71efb6919c5aadc5
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-06-11 15:09:38 +0200

    Testing ONC samples

---
+++ testing/integration_tests/test_oncoservice_samples.py
@@ -0,0 +1,38 @@
+import pytest
+from pathlib import Path
+from scripts.helpers import get_server_ip
+from shutil import copytree as sh_copytree
+from subprocess import run as subp_run
+import yaml
+
+
+@pytest.fixture()
+def setup_environment():
+    with open('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+        pipeline_dir: Path = Path(config['pipeline_dir'])
+        onco_seq_dir:Path = Path(config['oncoservice_novaseqx_dir']) / 'Runs'
+        test_onco_samples:Path = Path('/mnt/Novaseq/TSO_pipeline/test_runs/test_samples_oncoservice')
+
+    server_ip = get_server_ip()
+    pending_file = pipeline_dir.parent.parent / f'{server_ip}_PENDING.txt'
+    test_onco_run_seq_dir = onco_seq_dir / 'test_run_onco_nsqx'
+
+    if not test_onco_run_seq_dir.exists():
+        sh_copytree(str(test_onco_samples),str(test_onco_run_seq_dir))
+
+    yield
+
+    pending_file.unlink()
+
+
+def test_scheduling(setup_environment):
+    scheduling_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/scheduler.py -t'
+    subp_run(scheduling_call, check=True, shell=True)
+
+
+def test_processing():
+    processing_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py -t'
+
+    for i in range(2):
+        subp_run(processing_call,check=True,shell=True)
