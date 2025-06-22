9b740d8d4e53e58531b8f1bd3c180c7dff48ab47
Matvii Mykhailichenko <103883982+matviimykhailichenko@users.noreply.github.com>
2025-06-22 09:47:38 +0200

    Driver script

--- scripts/drivers/analyse_manually.py
+++ scripts/drivers/analyse_manually.py
@@ -3,19 +3,8 @@ from pathlib import Path
 from datetime import datetime
 from shutil import copytree as sh_copytree, copy2 as sh_copy
 from subprocess import run as subp_run
-import os
 
 
-# Python 3.6 shenanigans
-def copytree_36(src, dst):
-    os.makedirs(dst, exist_ok=True)  # make sure destination exists
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            sh_copytree(s, d, symlinks=False, ignore=None)
-        else:
-            sh_copy(s, d)
 
 
 def main():
@@ -31,7 +20,7 @@ def main():
     parser.add_argument(
         "--sample_ids",
         type=str,
-        required=True,
+        required=False,
         help="Path to the run/sample directory"
     )
 
@@ -39,7 +28,14 @@ def main():
         "--input_type",
         type=str,
         required=True,
-        help="Path to the run/sample directory"
+        help="Type of the input: run/sample"
+    )
+
+    parser.add_argument(
+        "--tag",
+        type=str,
+        required=True,
+        help="Tag of the input"
     )
 
     args = parser.parse_args()
@@ -47,30 +43,40 @@ def main():
     run_dir = Path(args.run_dir)
 
     input_type = str(args.input_type)
-    if input_type == 'samples':
-        input_dir = run_dir / 'Analysis/1/Data/BCLConvert/fastq'
-
-    sample_ids = str(args.sample_ids)
-    sample_list = [s.strip() for s in sample_ids.split(',')]
+    tag = str(args.tag)
+    dragen_script = Path('/usr/local/bin/DRAGEN_TruSight_Oncology_500_ctDNA.sh')
+    run_name = f"{datetime.today().strftime('%y%m%d')}_TSO500_Onco"
     flowcell_name = run_dir.name
     input_staging_dir = Path('/staging/tmp') / flowcell_name
-    run_name = f"{datetime.today().strftime('%y%m%d')}_TSO500_Onco"
     analysis_dir = input_staging_dir / run_name
-    sample_sheet = run_dir / 'SampleSheet_Analysis.csv'
-    results_dir = Path('/mnt/NovaseqXplus/07_Oncoservice/Analyseergebnisse') / run_name
-    dragen_script = Path('/usr/local/bin/DRAGEN_TruSight_Oncology_500_ctDNA.sh')
+    if input_type == 'samples':
+        input_dir = run_dir / 'Analysis/1/Data/BCLConvert/fastq'
+        sample_ids = str(args.sample_ids)
+        sample_list = [s.strip() for s in sample_ids.split(',')]
+        sample_sheet = run_dir / 'SampleSheet_Analysis.csv'
+        dragen_call = f'{str(dragen_script)} --fastqFolder {str(input_staging_dir)}  --sampleSheet {str(sample_sheet)} --sampleIDs {str(sample_ids)} --analysisFolder {str(analysis_dir)}'
+    elif input_type == 'run':
+        sample_sheet = run_dir / 'SampleSheet.csv'
+        dragen_call = f'{str(dragen_script)} --fastqFolder {str(input_staging_dir)}  --sampleSheet {str(sample_sheet)} --analysisFolder {str(analysis_dir)}'
+    if tag == 'CBM':
+        results_dir = Path('/mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen') / flowcell_name / flowcell_name
+    elif tag == 'ONC':
+        results_dir = Path('/mnt/NovaseqXplus/07_Oncoservice/Analyseergebnisse') / run_name
 
     print(f"Staging the run {run_name}.")
     input_staging_dir.mkdir(parents=True, exist_ok=True)
-    # for sample_id in sample_list:
-    #     for fastq_file in input_dir.glob(f'{sample_id}_*.fastq.gz'):
-    #         dst_file = input_staging_dir / fastq_file.name
-    #         sh_copy(fastq_file, dst_file)
 
+    if input_type == 'sample':
+        for sample_id in sample_list:
+            for fastq_file in input_dir.glob(f'{sample_id}_*.fastq.gz'):
+                dst_file = input_staging_dir / fastq_file.name
+                sh_copy(fastq_file, dst_file)
+
+    elif input_type == 'run':
+        sh_copy(str(run_dir), input_staging_dir/flowcell_name)
     print(f"Staging completed! Running the TSO500 script for the run {run_name}.")
 
-    dragen_call = f'{str(dragen_script)} --fastqFolder {str(input_staging_dir)}  --sampleSheet {str(sample_sheet)} --sampleIDs {str(sample_ids)} --analysisFolder {str(analysis_dir)}'
-    # subp_run(dragen_call, check=True, shell=True)
+    subp_run(dragen_call, check=True, shell=True)
 
     print(f"TSO500 script completed! Transferring results for the run {run_name}.")
 
