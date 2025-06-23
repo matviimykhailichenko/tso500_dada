280493002cabbfc19f82a87d9359aa142b8a899a
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-06-23 10:23:53 +0200

    Testing with docker

--- scripts/drivers/analyse_manually.py
+++ scripts/drivers/analyse_manually.py
@@ -6,7 +6,6 @@ from subprocess import run as subp_run
 
 
 
-
 def main():
     parser = argparse.ArgumentParser(description="Process sample subset from arrival directory.")
 
@@ -49,7 +48,7 @@ def main():
     flowcell_name = run_dir.name
     input_staging_dir = Path('/staging/tmp') / flowcell_name
     analysis_dir = input_staging_dir / run_name
-    if input_type == 'samples':
+    if input_type == 'sample':
         input_dir = run_dir / 'Analysis/1/Data/BCLConvert/fastq'
         sample_ids = str(args.sample_ids)
         sample_list = [s.strip() for s in sample_ids.split(',')]
@@ -57,7 +56,7 @@ def main():
         dragen_call = f'{str(dragen_script)} --fastqFolder {str(input_staging_dir)}  --sampleSheet {str(sample_sheet)} --sampleIDs {str(sample_ids)} --analysisFolder {str(analysis_dir)}'
     elif input_type == 'run':
         sample_sheet = run_dir / 'SampleSheet.csv'
-        dragen_call = f'{str(dragen_script)} --run_Folder {str(input_staging_dir)}  --sampleSheet {str(sample_sheet)} --analysisFolder {str(analysis_dir)}'
+        dragen_call = f'{str(dragen_script)} --runFolder {str(input_staging_dir)}  --sampleSheet {str(sample_sheet)} --analysisFolder {str(analysis_dir)}'
     if tag == 'CBM':
         results_dir = Path('/mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen') / flowcell_name / flowcell_name
     elif tag == 'ONC':
@@ -72,6 +71,8 @@ def main():
                 dst_file = input_staging_dir / fastq_file.name
                 sh_copy(fastq_file, dst_file)
     elif input_type == 'run':
+        rsync_call = f'rsync -ra "{run_dir}/" "{input_staging_dir}/"'
+        subp_run(rsync_call, check=True, shell=True)
         sh_copytree(str(run_dir), input_staging_dir)
     print(f"Staging completed! Running the TSO500 script for the run {run_name}.")
 
