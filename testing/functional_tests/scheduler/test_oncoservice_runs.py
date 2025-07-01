50e68b0be043c92017d7a824cb49bbe1c55c69d1
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-01 10:49:09 +0200

    Testing scheduling with new logic

--- testing/functional_tests/scheduler/test_oncoservice_runs.py
+++ testing/functional_tests/scheduler/test_oncoservice_runs.py
@@ -7,7 +7,7 @@ import yaml
 
 @pytest.fixture()
 def setup_environment():
-    with open('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/config.yaml', 'r') as file:
+    with open('/mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
         onco_seq_dir:Path = Path(config['oncoservice_sequencing_dir'] +'_TEST') / 'Runs'
         test_onco_run_1:Path = Path('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/testing/integration_tests/mock/test_run_oncoservice_1')
@@ -24,6 +24,6 @@ def setup_environment():
 
 
 def test_scheduler(setup_environment):
-    scheduler_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/scheduler.py -t'
+    scheduler_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/scripts/scheduler.py -t'
     for i in range(1):
         subp_run(scheduler_call,check=True,shell=True)
