4b123bd9897747eed108f1880f99cef88daa7457
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-01 11:50:05 +0200

    Testing ONC samples

--- testing/integration_tests/test_oncoservice_samples/test_oncoservice_samples.py
+++ testing/integration_tests/test_oncoservice_samples/test_oncoservice_samples.py
@@ -11,8 +11,8 @@ def setup_environment(request):
     with open('/mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
         onco_seq_dir:Path = Path(config['oncoservice_novaseqx_dir'] + '_TEST') / 'Runs'
-        test_onco_samples:Path = Path('/mnt/NovaseqXplus/TSO_pipeline/test_runs/test_samples_oncoservice')
-    test_onco_run_seq_dir = onco_seq_dir / 'test_run_onco_nsqx'
+        test_onco_samples:Path = Path('/mnt/Novaseq/TSO_pipeline/test_runs/test_samples_oncoservice')
+    test_onco_run_seq_dir = onco_seq_dir / 'test_samples'
     pending_blank: Path = Path('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/testing/functional_tests/scheduler/PENDING_blank.txt')
     pending_file: Path = Path('/mnt/NovaseqXplus/TSO_pipeline/10.200.214.104_PENDING.txt')
 
