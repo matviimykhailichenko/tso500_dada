2fcacad0d640ce2655159629dfe64538bcec46f9
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-02 09:52:50 +0200

    Testing CBM samples

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -4,7 +4,7 @@ from shutil import which as sh_which, rmtree as sh_rmtree, copy as sh_copy, move
 from subprocess import run as subp_run, PIPE as subp_PIPE, CalledProcessError
 from typing import Optional
 import yaml
-from scripts.logging_ops import notify_bot, notify_pipeline_status
+from logging_ops import notify_bot, notify_pipeline_status
 from datetime import datetime
 import pandas as pd
 from filelock import FileLock
