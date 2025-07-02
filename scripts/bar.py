b6a8e02bc4047b57f0b677be79faa224ae513e2f
Matvii Mykhailichenko <103883982+matviimykhailichenko@users.noreply.github.com>
2025-07-02 16:31:09 +0200

    Testing CBM samples

--- scripts/bar.py
+++ scripts/bar.py
@@ -1,4 +1,19 @@
-result = "10.200.214.104 172.17.0.1"
-ips = result.split()
-server_ip = ips[-2]
-print(server_ip)
+from subprocess import run as subp_run, PIPE as subp_PIPE, CalledProcessError
+
+
+
+def get_server_ip() -> str:
+    try:
+        call = "hostname -I"
+        result = subp_run(call, shell=True, check=True, text=True, capture_output=True)
+        server_ip = result.stdout.split(' ')[-2]
+
+    except CalledProcessError as e:
+        message = f"Failed to retrieve server's ID: {e.stderr}"
+        raise RuntimeError(message)
+
+    return server_ip
+
+
+
+print(get_server_ip())
