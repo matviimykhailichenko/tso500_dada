94910e4a064f7df2a95c9b4ca2e639dea4316939
Matvii Mykhailichenko <103883982+matviimykhailichenko@users.noreply.github.com>
2025-07-02 16:24:28 +0200

    Testing CBM samples

--- scripts/bar.py
+++ scripts/bar.py
@@ -1,3 +1,4 @@
-sample_ids = ['Sample_1-CBM', 'Sample_1-CBM']
-tags = [s.split("-", 1)[1].split("_", 1)[0] for s in sample_ids]
-print(tags)
+result = "10.200.214.104 172.17.0.1"
+ips = result.split()
+server_ip = ips[-2]
+print(server_ip)
