7b7819c025ebd6a7f124ed87cc883cb9314345c2
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-02 09:47:22 +0200

    Testing CBM samples

--- testing/functional_tests/scheduler/test_oncoservice_samples.py
+++ testing/functional_tests/scheduler/test_oncoservice_samples.py
@@ -31,6 +31,6 @@ def setup_environment():
 
 
 def test_scheduler(setup_environment):
-    scheduler_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/scheduler.py -t'
+    scheduler_call = 'python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/scheduler.py -t'
     for i in range(1):
         subp_run(scheduler_call,check=True,shell=True)
