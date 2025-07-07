eceb2718b0c412ccc0e88819b616cb479db51d20
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-07 10:52:59 +0200

    Adding research samples

--- testing/tso500_script_sub.sh
+++ testing/tso500_script_sub.sh
@@ -25,6 +25,9 @@ while [[ $# -gt 0 ]]; do
       IFS=',' read -ra SAMPLE_IDS <<< "$2"
       shift 2
       ;;
+    --sampleSheet)
+      shift 2
+      ;;
     *)
       echo "Unknown option: $1"
       exit 1
