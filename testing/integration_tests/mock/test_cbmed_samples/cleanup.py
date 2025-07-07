ed366ff5ae6eb306e1b82dc06458ff339771c9ee
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-07 11:24:59 +0200

    Adding research samples

--- testing/integration_tests/mock/test_cbmed_samples/cleanup.py
+++ testing/integration_tests/mock/test_cbmed_samples/cleanup.py
@@ -1,7 +1,7 @@
-from pathlib import Path
+from shutil import rmtree as sh_rmtree
 
 
 
-Path('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/20250707_LH00803_0012_B232KMCLT3').unlink()
-Path('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/flowcells/20250707_LH00803_0012_B232KMCLT3').unlink()
-Path('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/dragen/20250707_LH00803_0012_B232KMCLT3').unlink()
+sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/20250707_LH00803_0012_B232KMCLT3')
+sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/flowcells/20250707_LH00803_0012_B232KMCLT3')
+sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/dragen/20250707_LH00803_0012_B232KMCLT3')
