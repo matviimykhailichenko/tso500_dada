4a107773f320aac698bfc5e4b351fe0523ad700c
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-07 11:23:05 +0200

    Adding research samples

---
+++ testing/integration_tests/mock/test_cbmed_samples/cleanup.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+
+Path('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/20250707_LH00803_0012_B232KMCLT3').unlink()
+Path('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/flowcells/20250707_LH00803_0012_B232KMCLT3').unlink()
+Path('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/dragen/20250707_LH00803_0012_B232KMCLT3').unlink()
