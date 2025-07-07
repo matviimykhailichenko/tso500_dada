3af62d8a23fbbcb498f52d974a1c4a2d08d83f94
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-07 14:55:43 +0200

    Adding research samples

--- testing/integration_tests/test_cbmed_runs.py
+++ testing/integration_tests/test_cbmed_runs.py
@@ -1,6 +1,5 @@
 import pytest
 from pathlib import Path
-from scripts.helpers import get_server_ip
 from shutil import copy as sh_copy, copytree as sh_copytree
 from subprocess import run as subp_run
 import yaml
@@ -8,13 +7,13 @@ import yaml
 
 @pytest.fixture()
 def setup_environment():
-    with open('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/config.yaml', 'r') as file:
+    with open('/mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
         pipeline_dir: Path = Path(config['pipeline_dir'])
         cbmed_seq_dir: Path = Path(config['cbmed_nsq6000_dir'] + '_TEST')
         test_pending_file = Path('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/testing/integration_tests/PENDING_cbmed_runs.txt')
         test_cbmed_run: Path = Path('/mnt/Novaseq/TSO_pipeline/test_runs/test_run_cbmed')
-    server_ip = get_server_ip()
+    server_ip = '10.200.215.35'
     queue_file = pipeline_dir.parent.parent / f'{server_ip}_QUEUE.txt'
     pending_file = pipeline_dir.parent.parent / f'{server_ip}_PENDING.txt'
     test_cbmed_run_seq_dir = cbmed_seq_dir / 'test_run_cbmed'
@@ -34,6 +33,14 @@ def setup_environment():
     pending_file.unlink()
 
 
-def test_processing(setup_environment):
-    processing_call = 'conda run -n tso500_dragen_pipeline python3 /mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py'
-    subp_run(processing_call,check=True,shell=True)
+@pytest.mark.dependency(name="scheduling")
+def test_scheduling(setup_environment):
+    scheduling_call = 'python3 /mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/scripts/scheduler.py -t'
+    subp_run(scheduling_call, check=True, shell=True)
+
+
+@pytest.mark.dependency(depends=["scheduling"])
+def test_processing():
+    processing_call = 'python3 /mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py -t'
+    for i in range(2):
+        subp_run(processing_call,check=True,shell=True)
