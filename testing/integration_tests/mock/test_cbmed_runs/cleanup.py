3252878bb039a7982cefb408d2a82482c02a45a1
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-11 09:58:00 +0200

    Testing CBmed samples

---
+++ testing/integration_tests/mock/test_cbmed_runs/cleanup.py
@@ -0,0 +1,6 @@
+from shutil import rmtree as sh_rmtree
+
+
+
+sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/test_run_cbmed_2')
+sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/test_run_cbmed_1')
