ef66f987ba27d95b5b13bcf5f7e714773ab8e653
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-11 11:17:29 +0200

    Testing CBmed samples

--- testing/integration_tests/mock/test_cbmed_runs/cleanup.py
+++ testing/integration_tests/mock/test_cbmed_runs/cleanup.py
@@ -1,8 +1,13 @@
-from shutil import rmtree as sh_rmtree
+import shutil
 
-
+def sh_rmtree(path):
+    try:
+        shutil.rmtree(path)
+    except FileNotFoundError:
+        pass
 
 sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/test_run_cbmed_2')
 sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/test_run_cbmed_1')
 sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/flowcells/250213_A01664_0452_AH2J5VDMX2')
 sh_rmtree('/mnt/CBmed_NAS3/Genomics/TSO500_liquid_TEST/dragen/250213_A01664_0452_AH2J5VDMX2')
+
