0cb6031beb1a56a8d264be2213647ac1909f42e4
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-07-14 16:17:14 +0200

    Testing CBmed samples

---
+++ scripts/drivers/generate_metrics_manually.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from helpers import merge_metrics
+
+
+
+paths = {}
+paths['results_dir'] = (Path('/mnt/NovaseqXplus/07_Oncoservice/Analyseergebnisse/250710_TSO500_Onco'))
+print(merge_metrics(paths=paths))
