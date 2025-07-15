4aa6ec522a899820fd786302e54b37ec99f3cf3d
Matvii Mykhailichenko <103883982+matviimykhailichenko@users.noreply.github.com>
2025-07-15 15:58:35 +0200

    Archiving results

--- testing/integration_tests/archiving/cleanup.py
+++ testing/integration_tests/archiving/cleanup.py
@@ -4,3 +4,16 @@ from pathlib import Path
 
 for sample_dir in Path('/mnt/NovaseqXplus/07_Oncoservice_TEST/Analyseergebnisse/250710_TSO500_Onco/Logs_Intermediates/DragenCaller').iterdir():
     sample_dir.glob('*.cram').unlink()
+
+base_dir = Path('/mnt/NovaseqXplus/07_Oncoservice_TEST/Analyseergebnisse/250710_TSO500_Onco')
+
+
+
+if (base_dir / 'ARCHIVED.txt').exists():
+    (base_dir / 'ARCHIVED.txt').unlink()
+
+if not (base_dir / 'ANALYZED.txt').exists():
+    (base_dir / 'ANALYZED.txt').unlink()
+
+if (base_dir / 'ARCHIVING_FAILED.txt').exists():
+    (base_dir / 'ARCHIVING_FAILED.txt').unlink()
