a46b69986c19cd3ddd04022323c25fdf26ff71fe
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-08-14 09:49:45 +0200

    Testing crontab

--- sandbox/crontab/test_script.py
+++ sandbox/crontab/test_script.py
@@ -1,9 +1,11 @@
 import subprocess
+from datetime import datetime
 
-from scripts.logging_ops import notify_bot
+log_file = "/mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/sandbox/crontab/error_log.txt"  # change to your desired location
 
 cmd = '/usr/local/bin/DRAGEN_TruSight_Oncology_500_ctDNA.sh -h'
 try:
     subprocess.run(cmd, shell=True, check=True)
 except Exception as e:
-    notify_bot(f'The command had failed: {e}')
+    with open(log_file, "a") as f:
+        f.write(f"{datetime.now()} - The command had failed: {e}\n")
