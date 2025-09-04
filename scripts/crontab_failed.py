29270a7bd57a19d5896bf1e5fbc7796b310c3be6
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-09-04 10:46:22 +0200

    Better messaging when crontab fails

--- scripts/crontab_failed.py
+++ scripts/crontab_failed.py
@@ -1,4 +1,19 @@
 from logging_ops import notify_bot
+import sys
+
 
 msg = f'A crontab command had failed due to unexpected reasons.'
 notify_bot(msg)
+
+
+def main():
+    if len(sys.argv) < 2:
+        notify_bot("Usage: crontab_failed.py <error_message>")
+        sys.exit(1)
+
+    error_message = sys.argv[2]
+
+    notify_bot(f"The crontab had failed with message: {error_message}")
+
+if __name__ == "__main__":
+    main()
