31aade31cddf4bb4c12c0a84e0dcfe4aeb82a352
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-09-04 11:06:02 +0200

    Putting the current version of the pipeline to production

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -21,7 +21,7 @@ def main():
     args = parser.parse_args()
     testing = args.testing
 
-    repo_root = '/mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor'
+    repo_root = '/mnt/NovaseqXplus/TSO_pipeline/03_Production/pure-python-refactor'
 
     with open(f'{repo_root}/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
