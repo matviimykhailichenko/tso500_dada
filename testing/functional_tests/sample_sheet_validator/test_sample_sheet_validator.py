bf0451e1b14ec00f1a03d0c9c2378bf1ffe6bad8
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-09-05 12:49:21 +0200

    Integrating sample sheet validation in scheduler.py and testing it

--- testing/functional_tests/sample_sheet_validator/test_sample_sheet_validator.py
+++ testing/functional_tests/sample_sheet_validator/test_sample_sheet_validator.py
@@ -47,5 +47,5 @@ def setup_environment():
 
 def test_scheduler(setup_environment):
     scheduler_call = f'/staging/env/tso500_dragen_pipeline/bin/python3 {repo_root}/scripts/scheduler.py -t'
-    for i in range(11):
+    for i in range(1):
         subp_run(scheduler_call,check=True,shell=True)
