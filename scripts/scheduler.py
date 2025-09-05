8df46c49975b2f53f5b1b6aefbaee75474356cd6
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-09-05 11:10:52 +0200

    Integrating sample sheet validation in scheduler.py and testing it

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -87,7 +87,7 @@ def main():
     ok, msg = validate_samplesheet(repo_root=repo_root, input_type=input_type, sample_sheet=sample_sheet, config=config)
     if not ok:
         message = f'Samplesheet validation failed:\n'
-                     f'{msg}'
+                 f'{msg}'
         notify_bot(message)
         RuntimeError(message)
 
