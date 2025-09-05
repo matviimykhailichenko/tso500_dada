f20ee865919b2526deff2c789a3c71421a44b770
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-09-05 11:23:39 +0200

    Integrating sample sheet validation in scheduler.py and testing it

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -66,7 +66,7 @@ def main():
                 input_type = 'run'
                 input_path = scan_dir_nsq6000(flowcell_dir=flowcell_dir, repo_root=repo_root)
 
-            if not input_path:
+            if not input_path or not flowcell_dir:
                 continue
 
             if input_path:
