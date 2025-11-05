3bd010fe43ba581adbb828b41692268d67b2e139
Matvii Mykhailichenko <103883982+matviimykhailichenko@users.noreply.github.com>
2025-11-05 15:26:08 +0100

    Test script

--- sandbox/process_5_samples.py
+++ sandbox/process_5_samples.py
@@ -1,7 +1,24 @@
 from subprocess import run
+import pytest
+from pathlib import Path
+from subprocess import CalledProcessError, check_output as subp_check_output
+import yaml
 
 
-cmd = 'python3 /mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/scripts/scheduler.py -t'
+
+def get_repo_root() -> str:
+    script_path = Path(__file__).parent
+    try:
+        root = subp_check_output(
+            f"cd {script_path} && git rev-parse --show-toplevel",
+            text=True, shell=True
+        ).strip()
+        return root
+    except CalledProcessError:
+        raise RuntimeError("Not inside a git repository")
+
+repo_root = get_repo_root()
+cmd = f'/staging/env/tso500_dragen_pipeline/bin/python3 {repo_root}/scripts/processing.py'
 
 for i in range(5):
     run(cmd, check=True, shell=True)
