97eff12edccbf1bf02205d206dae1d1b3404bf4c
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-07 14:30:14 +0100

    Test fix

--- testing/integration_tests/mock/test_cbmed_runs/test_cbmed_runs.py
+++ testing/integration_tests/mock/test_cbmed_runs/test_cbmed_runs.py
@@ -1,24 +1,28 @@
 import pytest
 from pathlib import Path
-from shutil import copy as sh_copy, copytree as sh_copytree, rmtree as sh_rmtree
-from subprocess import run as subp_run
+from shutil import copytree
+from subprocess import run
+from datetime import datetime
 import yaml
+from ....scripts.helpers import get_repo_root, get_server_ip
+
 
 
 @pytest.fixture()
 def setup_environment():
-    with open('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/config.yaml', 'r') as file:
+    repo_root = get_repo_root()
+    with open(f'{repo_root}/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
         pipeline_dir: Path = Path(config['pipeline_dir'])
         cbmed_seq_dir:Path = Path(config['cbmed_sequencing_dir'] + '_TEST')
-    test_cbmed_run_1:Path = Path('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/testing/integration_tests/mock/test_run_cbmed_1')
-    test_cbmed_run_2:Path = Path('/mnt/Novaseq/TSO_pipeline/01_Staging/pure-python-refactor/testing/integration_tests/mock/test_run_cbmed_2')
+    test_cbmed_run_1:Path = Path(f'{repo_root}/testing/integration_tests/mock/test_run_cbmed_1')
+    test_cbmed_run_2:Path = Path(f'{repo_root}/testing/integration_tests/mock/test_run_cbmed_2')
 
-    server_ip = '10.200.215.35'
+    server_ip = get_server_ip
     queue_file = pipeline_dir.parent.parent / f'{server_ip}_QUEUE.txt'
     pending_file = pipeline_dir.parent.parent / f'{server_ip}_PENDING.txt'
-    test_cbmed_run_seq_dir_1 = cbmed_seq_dir / 'test_run_cbmed_1'
-    test_cbmed_run_seq_dir_2 = cbmed_seq_dir / 'test_run_cbmed_2'
+    test_cbmed_run_seq_dir_1 = cbmed_seq_dir / f'{datetime.now().strftime("%y%m%d")}_BI_735_batch1'
+    test_cbmed_run_seq_dir_2 = cbmed_seq_dir / f'{datetime.now().strftime("%y%m%d")}_BI_735_batch2'
 
     if queue_file.exists():
         queue_file.unlink()
@@ -27,21 +31,22 @@ def setup_environment():
         pending_file.unlink()
 
     if not test_cbmed_run_seq_dir_1.exists():
-        sh_copytree(str(test_cbmed_run_1), str(test_cbmed_run_seq_dir_1))
+        copytree(str(test_cbmed_run_1), str(test_cbmed_run_seq_dir_1))
 
     if not test_cbmed_run_seq_dir_2.exists():
-        sh_copytree(str(test_cbmed_run_2), str(test_cbmed_run_seq_dir_2))
+        copytree(str(test_cbmed_run_2), str(test_cbmed_run_seq_dir_2))
 
 
 @pytest.mark.dependency(name='scheduler')
 def test_scheduler(setup_environment):
-    scheduler_call = 'python3 /mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/scripts/scheduler.py -t'
-    subp_run(scheduler_call, check=True, shell=True)
+    repo_root = get_repo_root()
+    cmd = f'/staging/env/tso500_dragen_pipeline/bin/python3 {repo_root}/scripts/scheduler.py -t'
+    run(cmd, check=True, shell=True)
 
 
 @pytest.mark.dependency(depends=['scheduler'])
 def test_processing():
-    processing_call = 'python3 /mnt/NovaseqXplus/TSO_pipeline/01_Staging/pure-python-refactor/scripts/processing.py -t -tf'
-
+    repo_root = get_repo_root()
+    cmd = f'/staging/env/tso500_dragen_pipeline/bin/python3 {repo_root}/scripts/processing.py -t -tf'
     for i in range(2):
-        subp_run(processing_call,check=True,shell=True)
+        run(cmd,check=True,shell=True)
