def6a9739317077eea437d62ebae4a7503f25283
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-11 16:24:51 +0100

    New script
    (cherry picked from commit c312645bf5c30ccd3081f315c07f7060eb785cfd)
    
    # Conflicts:
    #	testing/helpers.py

--- testing/helpers.py
+++ testing/helpers.py
@@ -1,15 +1,13 @@
 from crontab import CronTab
 
-
-
 def enable_testing_mode(server_ip):
     last_part = server_ip.split('.')[-1]
     crontab_testing = f'/mnt/NovaseqXplus/TSO_pipeline/03_Production/tso500_dragen_pipeline/files/crontabs/crontab_{last_part}_testing'
     cron = CronTab(tabfile=crontab_testing)
-    cron.write_to_user(user=True)
+    cron.write_to_user(user=True)  # <--- this activates it for the current user
 
 def disable_testing_mode(server_ip, repo_root):
     last_part = server_ip.split('.')[-1]
     crontab_production = f'{repo_root}/files/crontabs/crontab_{last_part}'
     cron = CronTab(tabfile=crontab_production)
-    cron.write_to_user(user=True)
+    cron.write_to_user(user=True)  # <--- restores normal crontab
