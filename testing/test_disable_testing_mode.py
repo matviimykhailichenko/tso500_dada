7905a66be1f43f4deed22fa83f4e9f0d75818834
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-11 16:25:35 +0100

    New script

--- testing/test_disable_testing_mode.py
+++ testing/test_disable_testing_mode.py
@@ -1,4 +1,4 @@
-from testing.helpers import disable_testing_mode
+from helpers import disable_testing_mode
 
 server_ip = "10.200.214.104"
 disable_testing_mode(server_ip='server_ip', repo_root='/mnt/NovaseqXplus/TSO_pipeline/03_Production/tso500_dragen_pipeline')
