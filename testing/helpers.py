c312645bf5c30ccd3081f315c07f7060eb785cfd
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-11 16:24:51 +0100

    New script

--- testing/helpers.py
+++ testing/helpers.py
@@ -1,12 +1,13 @@
 from crontab import CronTab
 
 def enable_testing_mode(server_ip):
-    last_part = '.'.join(server_ip.split('.')[3:])
-    cron = CronTab(user=True, tabfile=f'/mnt/NovaseqXplus/TSO_pipeline/03_Production/tso500_dragen_pipeline/files/crontabs/crontab_{last_part}_testing')
-    cron.write()
+    last_part = server_ip.split('.')[-1]
+    tabfile = f'/mnt/NovaseqXplus/TSO_pipeline/03_Production/tso500_dragen_pipeline/files/crontabs/crontab_{last_part}_testing'
+    cron = CronTab(tabfile=tabfile)
+    cron.write_to_user(user=True)  # <--- this activates it for the current user
 
 def disable_testing_mode(server_ip, repo_root):
-    last_part = '.'.join(server_ip.split('.')[3:])
-    cron = CronTab(user=True, tabfile=f'{repo_root}/files/crontabs/crontab_{last_part}')
-    cron.write()
-
+    last_part = server_ip.split('.')[-1]
+    tabfile = f'{repo_root}/files/crontabs/crontab_{last_part}'
+    cron = CronTab(tabfile=tabfile)
+    cron.write_to_user(user=True)  # <--- restores normal crontab
