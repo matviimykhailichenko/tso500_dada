2ef1cd1b949629d4862b28f72065934070e5c7f0
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-13 10:11:16 +0100

    Env files

--- testing/integration_tests/mock/test_cbmed_runs.py
+++ testing/integration_tests/mock/test_cbmed_runs.py
@@ -1,11 +1,10 @@
 import pytest
 from pathlib import Path
-from shutil import copytree
+from shutil import copy as sh_copy, copytree as sh_copytree
 from subprocess import run
 from datetime import datetime
 import yaml
 from ..scripts.helpers import get_repo_root, get_server_ip
-
 
 
 @pytest.fixture()
@@ -14,34 +13,33 @@ def setup_environment():
     with open(f'{repo_root}/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
         pipeline_dir: Path = Path(config['pipeline_dir'])
-        cbmed_seq_dir:Path = Path(config['cbmed_sequencing_dir'] + '_TEST')
-    test_ns6000_run: Path = pipeline_dir / 'test_runs/mock/test_run_ns6000'
-
-    server_ip = get_server_ip
+        cbmed_seq_dir: Path = Path(config['cbmed_sequencing_dir'] + '_TEST')
+        pending_blank: Path = Path(f'{repo_root}/testing/files/PENDING_blank.txt')
+        test_cbmed_run: Path = Path(f'{pipeline_dir}/test_runs/mock/test_run_ns6000')
+    server_ip = get_server_ip()
     queue_file = pipeline_dir.parent.parent / f'{server_ip}_QUEUE.txt'
     pending_file = pipeline_dir.parent.parent / f'{server_ip}_PENDING.txt'
     test_cbmed_run_seq_dir = cbmed_seq_dir / f'{datetime.now().strftime("%y%m%d")}_BI_735_batch1'
 
     if queue_file.exists():
-        queue_file.unlink()
+        sh_copy(str(pending_blank), str(pending_file))
 
     if pending_file.exists():
-        pending_file.unlink()
+        sh_copy(str(pending_blank),str(pending_file))
 
     if not test_cbmed_run_seq_dir.exists():
-        copytree(str(test_ns6000_run), str(test_cbmed_run_seq_dir))
+        sh_copytree(str(test_cbmed_run),str(test_cbmed_run_seq_dir))
 
 
-@pytest.mark.dependency(name='scheduler')
-def test_scheduler(setup_environment):
+@pytest.mark.dependency(name="scheduling")
+def test_scheduling(setup_environment):
     repo_root = get_repo_root()
     cmd = f'/staging/env/tso500_dragen_pipeline/bin/python3 {repo_root}/scripts/scheduler.py -t'
     run(cmd, check=True, shell=True)
 
 
-@pytest.mark.dependency(depends=['scheduler'])
+@pytest.mark.dependency(depends=["scheduling"])
 def test_processing():
     repo_root = get_repo_root()
-    cmd = f'/staging/env/tso500_dragen_pipeline/bin/python3 {repo_root}/scripts/processing.py -t -tf'
-    for i in range(2):
-        run(cmd,check=True,shell=True)
+    cmd = f'/staging/env/tso500_dragen_pipeline/bin/python3 {repo_root}/scripts/processing.py -t  -tf'
+    run(cmd,check=True,shell=True)
