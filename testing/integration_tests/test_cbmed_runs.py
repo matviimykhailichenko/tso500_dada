bccaad75220fbe5ec8d07cbad70acb6fc8ee7752
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-13 10:28:00 +0100

    Enable/disable testing mode

--- testing/integration_tests/test_cbmed_runs.py
+++ testing/integration_tests/test_cbmed_runs.py
@@ -5,11 +5,13 @@ from shutil import copy as sh_copy, copytree as sh_copytree
 from subprocess import run
 from datetime import datetime
 import yaml
-from ..scripts.helpers import get_repo_root, get_server_ip
+from ..scripts.helpers import get_repo_root, get_server_ip, enable_testing_mode, disable_testing_mode
 
 
 @pytest.fixture()
 def setup_environment():
+    server_ip = get_server_ip()
+    enable_testing_mode(server_ip=server_ip)
     repo_root = get_repo_root()
     with open(f'{repo_root}/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
@@ -17,7 +19,6 @@ def setup_environment():
         cbmed_seq_dir: Path = Path(config['cbmed_sequencing_dir'] + '_TEST')
         pending_blank: Path = Path(f'{repo_root}/files/PENDING_blank.txt')
         test_cbmed_run: Path = Path(f'{pipeline_dir}/test_runs/cbmed')
-    server_ip = get_server_ip()
     queue_file = pipeline_dir.parent.parent / f'{server_ip}_QUEUE.txt'
     pending_file = pipeline_dir.parent.parent / f'{server_ip}_PENDING.txt'
     test_cbmed_run_seq_dir = cbmed_seq_dir / f'{datetime.now().strftime("%y%m%d")}_BI_735_batch1'
@@ -30,6 +31,10 @@ def setup_environment():
 
     if not test_cbmed_run_seq_dir.exists():
         sh_copytree(str(test_cbmed_run),str(test_cbmed_run_seq_dir))
+
+    yield
+
+    disable_testing_mode(server_ip=server_ip, repo_root=repo_root)
 
 
 @pytest.mark.dependency(name="scheduling")
