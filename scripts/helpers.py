2bd33699ae72264774b31f623223ad4ee87e535c
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-13 12:05:07 +0100

    Enable/disable testing mode

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -99,7 +99,7 @@ def transfer_results_oncoservice(paths: dict, input_type: str, logger: Logger, t
 def transfer_results_cbmed(paths: dict, input_type: str, logger: Logger):
     cbmed_results_dir= paths['results_dir']
     flowcell = paths['flowcell']
-    flowcell_cbmed_dir= cbmed_results_dir / 'flowcells' / flowcell
+    flowcell_cbmed_dir = paths['cbmed_seq_dir']
     data_cbmed_dir= flowcell_cbmed_dir / flowcell
     dragen_cbmed_dir= cbmed_results_dir / 'dragen'
     run_name = paths['run_name']
