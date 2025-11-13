418ef047354b2a7b3889b619373c17788581b361
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-13 11:09:33 +0100

    Enable/disable testing mode

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -402,6 +402,7 @@ def setup_paths(repo_root: str, input_path: Path, input_type: str, tag: str, flo
         flowcell_dir_cbmed = paths['cbmed_seq_dir'] / flowcell / flowcell
         paths['analyzing_tag_flowcell_dir'] = flowcell_dir_cbmed / config.get('analyzing_tag')
         paths['analyzed_tag_flowcell_dir'] = flowcell_dir_cbmed / config.get('analyzing_tag')
+        paths['failed_tag_flowcell_dir'] = flowcell_dir_cbmed / config.get('failed_tag')
 
     return paths
 
