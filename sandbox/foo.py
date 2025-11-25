252a36d063a4462c49fe6ffe10beec29fdb7381d
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-25 14:17:37 +0100

    Real-time capturing of warnings

--- sandbox/foo.py
+++ sandbox/foo.py
@@ -1,1 +1,4 @@
+from time import sleep
 print("WARNING: Truncated cbcl file missing tile 1102 '/mnt/CBmed_NAS3/Genomics/RNAseq_liquid/flowcells_TEST/test_big_run/250829_A01664_0550_AH3MYWDMX2/Data/Intensities/BaseCalls/L001/C1.1/L001_1.cbcl': no-calls used for missing cycle across the lane+surface")
+
+sleep(10)
