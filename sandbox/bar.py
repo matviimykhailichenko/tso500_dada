edc0dc7dbdbf450a624fd37476a400e2984d37e6
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-25 13:51:16 +0100

    Real-time capturing of warnings

--- sandbox/bar.py
+++ sandbox/bar.py
@@ -1,8 +1,10 @@
 from subprocess import PIPE, Popen, STDOUT
 
-cmd = 'bcl-convert ' \
-      '--bcl-input-directory /mnt/CBmed_NAS3/Genomics/RNAseq_liquid/flowcells_TEST/test_big_run/250829_A01664_0550_AH3MYWDMX2 ' \
-      '--output-directory /staging/tmp/RNAseq_test'
+# cmd = 'bcl-convert ' \
+#       '--bcl-input-directory /mnt/CBmed_NAS3/Genomics/RNAseq_liquid/flowcells_TEST/test_big_run/250829_A01664_0550_AH3MYWDMX2 ' \
+#       '--output-directory /staging/tmp/RNAseq_test'
+
+cmd = 'python3 foo.py'
 
 proc = Popen(
     cmd,
