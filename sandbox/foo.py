9192a0886cd2ef06dd2e4ff07699365ffa52b430
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-25 14:05:06 +0100

    Real-time capturing of warnings

--- sandbox/foo.py
+++ sandbox/foo.py
@@ -1,1 +1,17 @@
-print('Literally WARNING')
+from subprocess import PIPE, Popen, STDOUT
+
+cmd = 'bcl-convert ' \
+      '--bcl-input-directory /mnt/CBmed_NAS3/Genomics/RNAseq_liquid/flowcells_TEST/test_big_run/250829_A01664_0550_AH3MYWDMX2 ' \
+      '--output-directory /staging/tmp/RNAseq_test'
+
+proc = Popen(
+    cmd,
+    stdout=PIPE,
+    stderr=STDOUT,
+    text=True,
+    bufsize=1,
+    shell=True
+)
+for line in proc.stdout:
+    print("RAW:", repr(line))     # <-- shows hidden chars
+    print(line, end='')           # normal print
