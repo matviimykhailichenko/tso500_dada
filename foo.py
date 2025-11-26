44e7425bded5e2522a36c65ad4a11254f57a6907
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-26 16:21:45 +0100

    Cleaning up

--- foo.py
+++
@@ -1,8 +0,0 @@
-import re
-
-m = re.fullmatch(r'\d{6}_A01664_\d{4}_[A-Z0-9]{10}', '250123_A01664_0443_AH2J5YDMX2')
-
-if m:
-    print(m.group())     # the actual matched string
-else:
-    print("No match")
