bc80fbf05559f4347238f0da2f3b303a155d876f
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-27 16:26:42 +0100

    Running tests on 104

--- sandbox/bar.py
+++ sandbox/bar.py
@@ -2,7 +2,7 @@ from subprocess import run
 
 
 diff_call = (
-        f'diff <(sort {'/mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen_TEST/20251127_BI_733_b01_s01/250213_A01664_0452_AH2J5VDMX2.sha256'}) <(sort {'/mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen_TEST/20251127_BI_733_b01_s01/250213_A01664_0452_AH2J5VDMX2_Results_HumGenNAS.sha256'})'
+        r'diff <(sort /mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen_TEST/20251127_BI_733_b01_s01/250213_A01664_0452_AH2J5VDMX2.sha256) <(sort /mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen_TEST/20251127_BI_733_b01_s01/250213_A01664_0452_AH2J5VDMX2_Results_HumGenNAS.sha256)'
     )
 stdout = run(diff_call, shell=True, capture_output=True,text=True, check=True, executable='/bin/bash').stdout.strip()
 if stdout is not None:
