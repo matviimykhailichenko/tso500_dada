d6fe92d03caedaf9d2c4fcce918738042fc24e49
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-27 16:27:04 +0100

    Running tests on 104

--- sandbox/bar.py
+++ sandbox/bar.py
@@ -1,11 +1,14 @@
 from subprocess import run
 
+f1 = "/mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen_TEST/20251127_BI_733_b01_s01/250213_A01664_0452_AH2J5VDMX2.sha256"
+f2 = "/mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen_TEST/20251127_BI_733_b01_s01/250213_A01664_0452_AH2J5VDMX2_Results_HumGenNAS.sha256"
 
-diff_call = (
-        r'diff <(sort /mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen_TEST/20251127_BI_733_b01_s01/250213_A01664_0452_AH2J5VDMX2.sha256) <(sort /mnt/CBmed_NAS3/Genomics/TSO500_liquid/dragen_TEST/20251127_BI_733_b01_s01/250213_A01664_0452_AH2J5VDMX2_Results_HumGenNAS.sha256)'
-    )
-stdout = run(diff_call, shell=True, capture_output=True,text=True, check=True, executable='/bin/bash').stdout.strip()
-if stdout is not None:
-    msg = f"Checksums in a CBmed run are different: {stdout}"
-    print(msg)
-    # raise RuntimeError(msg)
+diff_call = f"diff <(sort '{f1}') <(sort '{f2}')"
+
+result = run(diff_call, shell=True, capture_output=True, text=True,
+             executable="/bin/bash")
+
+stdout = result.stdout.strip()
+
+if stdout:
+    print(f"Checksums differ:\n{stdout}")
