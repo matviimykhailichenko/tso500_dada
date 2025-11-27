b8bb7da4baaf7505fd0a3d83cdf29e7af1f50965
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-11-27 16:35:53 +0100

    Cleaning up + running the final test

--- testing/integration_tests/test_cbmed_runs.py
+++ testing/integration_tests/test_cbmed_runs.py
@@ -1,4 +1,3 @@
-import sys
 import pytest
 from pathlib import Path
 from shutil import copy as sh_copy, copytree as sh_copytree
@@ -14,10 +13,10 @@ def setup_environment():
     repo_root = get_repo_root()
     with open(f'{repo_root}/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
-        pipeline_dir: Path = Path(config['pipeline_dir'])
-        cbmed_seq_dir: Path = Path(config['cbmed_sequencing_dir'] + '_TEST')
-        pending_blank: Path = Path(f'{repo_root}/files/PENDING_blank.txt')
-        test_cbmed_run: Path = Path(f'{pipeline_dir}/test_runs/cbmed')
+        pipeline_dir = Path(config['pipeline_dir'])
+        cbmed_seq_dir = Path(config['cbmed_sequencing_dir'] + '_TEST')
+        pending_blank = Path(f'{repo_root}/files/PENDING_blank.txt')
+        test_cbmed_run = Path(f'{pipeline_dir}/test_runs/run_ns6000')
     queue_file = pipeline_dir.parent.parent / f'{server_ip}_QUEUE.txt'
     pending_file = pipeline_dir.parent.parent / f'{server_ip}_PENDING.txt'
     test_cbmed_run_seq_dir = cbmed_seq_dir / f'{datetime.now().strftime("%y%m%d")}_BI_735_batch1'
@@ -30,6 +29,7 @@ def setup_environment():
 
     if not test_cbmed_run_seq_dir.exists():
         sh_copytree(str(test_cbmed_run),str(test_cbmed_run_seq_dir))
+
 
 @pytest.mark.dependency(name="scheduling")
 def test_scheduling(setup_environment):
