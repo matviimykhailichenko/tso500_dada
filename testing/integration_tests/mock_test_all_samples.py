5268a74614dd59d97d29f7c98965c4b3f3d63a31
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-12-01 10:07:20 +0100

    Quickfix for research

--- testing/integration_tests/mock_test_all_samples.py
+++ testing/integration_tests/mock_test_all_samples.py
@@ -4,7 +4,7 @@ from shutil import copytree, rmtree
 from subprocess import run
 from datetime import datetime
 import yaml
-from ..scripts.helpers import get_repo_root, get_server_ip
+from ..scripts.helpers import get_repo_root, get_server_ip, generate_illumia_string
 
 
 
@@ -18,8 +18,7 @@ def setup_environment():
     server_ip = get_server_ip
     queue_file = pipeline_dir.parent.parent / f'{server_ip}_QUEUE.txt'
     pending_file = pipeline_dir.parent.parent / f'{server_ip}_PENDING.txt'
-    date = datetime.now().strftime("%y%m%d")
-    test_run_mixed_runs_dir = Path(config['mixed_runs_dir'] + '_TEST') / f'{date}_TSO500_Onco'
+    test_run_mixed_runs_dir = Path(config['mixed_runs_dir'] + '_TEST') / generate_illumia_string()
 
     if queue_file.exists():
         queue_file.unlink()
